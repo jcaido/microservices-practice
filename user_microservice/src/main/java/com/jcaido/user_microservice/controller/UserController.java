775c9a622baa9a31b7a950033d2d9b4db746f6fb
Cristobal Rosa <crisrogar@gmail.com>
2023-10-17 18:06:39 +0200

    get all user controller user microservice

---
+++ user_microservice/src/main/java/com/jcaido/user_microservice/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.jcaido.user_microservice.controller;
+
+import com.jcaido.user_microservice.entity.User;
+import com.jcaido.user_microservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        List<User> users = userService.getAll();
+
+        if (users.isEmpty())
+            return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok(users);
+    }
+}
