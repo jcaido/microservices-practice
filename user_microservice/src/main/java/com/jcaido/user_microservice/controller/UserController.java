479c003a3bc5d745fe726785e66c9d2ec6b914cb
Cristobal Rosa <crisrogar@gmail.com>
2023-10-17 18:12:09 +0200

    get user by id controller user microservice

--- user_microservice/src/main/java/com/jcaido/user_microservice/controller/UserController.java
+++ user_microservice/src/main/java/com/jcaido/user_microservice/controller/UserController.java
@@ -5,6 +5,7 @@ import com.jcaido.user_microservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,15 @@ public class UserController {
             return ResponseEntity.noContent().build();
 
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable("id") int id) {
+        User user = userService.getUserById(id);
+
+        if (user == null)
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(user);
     }
 }
