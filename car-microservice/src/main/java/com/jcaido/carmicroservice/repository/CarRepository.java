21ad137d078da847c35a1811faf4b40150afd46b
Cristobal Rosa <crisrogar@gmail.com>
2023-10-18 18:46:41 +0200

    feat: getCarByUserId CarRepository car microservice

--- car-microservice/src/main/java/com/jcaido/carmicroservice/repository/CarRepository.java
+++ car-microservice/src/main/java/com/jcaido/carmicroservice/repository/CarRepository.java
@@ -4,6 +4,9 @@ import com.jcaido.carmicroservice.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
+    List<Car> findByUserId(int userId);
 }
