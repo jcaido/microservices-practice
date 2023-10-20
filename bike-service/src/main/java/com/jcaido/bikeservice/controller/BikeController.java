35ff38bbf2ecf4868658fa254e8815ce723a9a27
Cristobal Rosa <crisrogar@gmail.com>
2023-10-20 18:48:04 +0200

    feat: BikeController bike microservice

---
+++ bike-service/src/main/java/com/jcaido/bikeservice/controller/BikeController.java
@@ -0,0 +1,54 @@
+package com.jcaido.bikeservice.controller;
+
+import com.jcaido.bikeservice.entity.Bike;
+import com.jcaido.bikeservice.service.BikeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bike")
+public class BikeController {
+
+    @Autowired
+    BikeService bikeService;
+
+    @GetMapping
+    public ResponseEntity<List<Bike>> getAll() {
+        List<Bike> bikes = bikeService.getAll();
+
+        if (bikes.isEmpty())
+            return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok(bikes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Bike> getBikeById(@PathVariable("id") int id) {
+        Bike bike = bikeService.getBikeById(id);
+
+        if (bike == null)
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(bike);
+    }
+
+    @GetMapping("/byuser/{userId}")
+    public ResponseEntity<List<Bike>> getBikesByUserId(@PathVariable("userId") int userId) {
+        List<Bike> bikes = bikeService.getBikesByUserId(userId);
+
+        if (bikes.isEmpty())
+            return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok(bikes);
+    }
+
+    @PostMapping
+    public ResponseEntity<Bike> save(@RequestBody Bike bike) {
+        return ResponseEntity.ok(bikeService.save(bike));
+    }
+
+
+}
--- car-microservice/src/main/java/com/jcaido/carmicroservice/controller/CarController.java
+++ car-microservice/src/main/java/com/jcaido/carmicroservice/controller/CarController.java
@@ -2,7 +2,6 @@ package com.jcaido.carmicroservice.controller;
 
 import com.jcaido.carmicroservice.entity.Car;
 import com.jcaido.carmicroservice.service.CarService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
