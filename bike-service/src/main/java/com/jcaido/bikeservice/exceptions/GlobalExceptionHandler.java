4e7b5419420a8f6a76d58ae99d7a91a584712bcc
Cristobal Rosa <crisrogar@gmail.com>
2023-10-30 18:23:10 +0100

    feat: add GlobalExceptionHandler bike microservice

---
+++ bike-service/src/main/java/com/jcaido/bikeservice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.jcaido.bikeservice.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
+
+        ErrorDetails errorDetails = new ErrorDetails(exception.getMessage(), webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+}
