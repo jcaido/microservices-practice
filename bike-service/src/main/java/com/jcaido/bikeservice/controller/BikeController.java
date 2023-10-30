29604f00dbc6a2edeb1bd13f5d649691b43e338d
Cristobal Rosa <crisrogar@gmail.com>
2023-10-30 18:36:22 +0100

    refact: GetBikeById bike microservice

--- bike-service/src/main/java/com/jcaido/bikeservice/controller/BikeController.java
+++ bike-service/src/main/java/com/jcaido/bikeservice/controller/BikeController.java
@@ -3,6 +3,7 @@ package com.jcaido.bikeservice.controller;
 import com.jcaido.bikeservice.entity.Bike;
 import com.jcaido.bikeservice.service.BikeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.rsocket.context.RSocketPortInfoApplicationContextInitializer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,18 +19,14 @@ public class BikeController {
 
     @GetMapping
     public ResponseEntity<List<Bike>> getAll() {
-        
+
         return new ResponseEntity<>(bikeService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Bike> getBikeById(@PathVariable("id") int id) {
-        Bike bike = bikeService.getBikeById(id);
-
-        if (bike == null)
-            return ResponseEntity.notFound().build();
-
-        return ResponseEntity.ok(bike);
+        
+        return new ResponseEntity<>(bikeService.getBikeById(id), HttpStatus.OK);
     }
 
     @GetMapping("/byuser/{userId}")
--- bike-service/src/main/java/com/jcaido/bikeservice/service/BikeServiceImpl.java
+++ bike-service/src/main/java/com/jcaido/bikeservice/service/BikeServiceImpl.java
@@ -1,11 +1,13 @@
 package com.jcaido.bikeservice.service;
 
 import com.jcaido.bikeservice.entity.Bike;
+import com.jcaido.bikeservice.exceptions.ResourceNotFoundException;
 import com.jcaido.bikeservice.repository.BikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BikeServiceImpl implements BikeService{
@@ -22,7 +24,11 @@ public class BikeServiceImpl implements BikeService{
 
     @Override
     public Bike getBikeById(int id) {
-        return bikeRepository.findById(id).orElseThrow();
+        Optional<Bike> bike = bikeRepository.findById(id);
+        if (!bike.isPresent())
+            throw new ResourceNotFoundException("Bike don't exist");
+
+        return bike.orElseThrow();
     }
 
     @Override
