e86d678a5a2fc086496cf8a9e3e73f4fa6345a96
Cristobal Rosa <crisrogar@gmail.com>
2023-10-30 14:18:01 +0100

    refact: getAll, getById car microservice

---
+++ car-microservice/src/main/java/com/jcaido/carmicroservice/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.jcaido.carmicroservice.exceptions;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Data
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+    private String resourceName;
+    public ResourceNotFoundException(String resourceName) {
+        super(resourceName);
+        this.resourceName = resourceName;
+    }
+}
