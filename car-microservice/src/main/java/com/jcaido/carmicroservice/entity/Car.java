00a1a9412ccde7b263caa107cf1b3531478081e0
Cristobal Rosa <crisrogar@gmail.com>
2023-11-13 12:51:02 +0100

    feat: clean class Car.java car microservice

--- car-microservice/src/main/java/com/jcaido/carmicroservice/entity/Car.java
+++ car-microservice/src/main/java/com/jcaido/carmicroservice/entity/Car.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.aot.generate.GeneratedMethod;
 
 @Entity
 @Data
