0010db8f9ab3e07b55921881439f312cec83aa03
Cristobal Rosa <crisrogar@gmail.com>
2023-11-16 13:09:29 +0100

    feat: create AuthController auth microservice

---
+++ auth-service/src/main/java/com/jcaido/authservice/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.jcaido.authservice.controller;
+
+import com.jcaido.authservice.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+}
