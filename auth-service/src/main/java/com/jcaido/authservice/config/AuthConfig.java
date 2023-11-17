037e550c9e0548595bc14db935be7f390a2be1b8
Cristobal Rosa <crisrogar@gmail.com>
2023-11-17 14:20:18 +0100

    :feat: update gateway-service.yml config service

--- auth-service/src/main/java/com/jcaido/authservice/config/AuthConfig.java
+++ auth-service/src/main/java/com/jcaido/authservice/config/AuthConfig.java
@@ -2,6 +2,7 @@ package com.jcaido.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,7 +17,7 @@ public class AuthConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
