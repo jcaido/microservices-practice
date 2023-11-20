2e89f7b550d225e3acaab5cc9018ab6b5cca307d
Cristobal Rosa <crisrogar@gmail.com>
2023-11-20 13:12:08 +0100

    feat: create Bean AuthenticationManager AuthConfig auth microservice

--- auth-service/src/main/java/com/jcaido/authservice/config/AuthConfig.java
+++ auth-service/src/main/java/com/jcaido/authservice/config/AuthConfig.java
@@ -2,7 +2,8 @@ package com.jcaido.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,5 +25,10 @@ public class AuthConfig {
                         .requestMatchers("/auth/register", "/auth/token", "/auth/validate").permitAll()
                         .anyRequest().authenticated())
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
