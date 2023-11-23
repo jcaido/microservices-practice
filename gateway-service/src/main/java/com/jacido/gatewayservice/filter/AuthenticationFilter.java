6a43dc00bdc255fe22dace78f43a87ba0d021e45
Cristobal Rosa <crisrogar@gmail.com>
2023-11-23 13:39:24 +0100

    feat: Override method apply AuthenticationFilter gateway service

--- gateway-service/src/main/java/com/jacido/gatewayservice/filter/AuthenticationFilter.java
+++ gateway-service/src/main/java/com/jacido/gatewayservice/filter/AuthenticationFilter.java
@@ -4,6 +4,7 @@ import com.jacido.gatewayservice.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -21,7 +22,26 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
 
     @Override
     public GatewayFilter apply(Config config) {
-        return null;
+        return ((exchange, chain) -> {
+            if (routeValidator.isSecured.test(exchange.getRequest())) {
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("missing authorization header");
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+
+                try {
+                    jwtUtil.validateToken(authHeader);
+                } catch(Exception e) {
+                    System.out.println("invalid access...!");
+                    throw new RuntimeException("unauthorized access to application");
+                }
+            }
+            return chain.filter(exchange);
+        });
     }
 
     public static class Config {
