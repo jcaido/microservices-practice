30bd36d95460e831e913073efa06b4c93c1e3ba7
Cristobal Rosa <crisrogar@gmail.com>
2023-11-23 13:10:46 +0100

    feat: inject JwtUtil AuthenticationFilter gateway service

--- gateway-service/src/main/java/com/jacido/gatewayservice/filter/AuthenticationFilter.java
+++ gateway-service/src/main/java/com/jacido/gatewayservice/filter/AuthenticationFilter.java
@@ -1,11 +1,17 @@
 package com.jacido.gatewayservice.filter;
 
+import com.jacido.gatewayservice.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
 
 
     @Override
