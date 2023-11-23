2ee4414aeed50474e17b0bf244fedd5feabdcab3
Cristobal Rosa <crisrogar@gmail.com>
2023-11-23 12:46:35 +0100

    feat: create class JwtUtil gateway service

---
+++ gateway-service/src/main/java/com/jacido/gatewayservice/util/JwtUtil.java
@@ -0,0 +1,9 @@
+package com.jacido.gatewayservice.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+}
