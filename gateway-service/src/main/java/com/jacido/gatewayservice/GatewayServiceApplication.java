692a59b735151010d606c4f365d460a962a72101
Cristobal Rosa <crisrogar@gmail.com>
2023-11-28 13:33:21 +0100

    feat: update handle Exception gateway service

--- gateway-service/src/main/java/com/jacido/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/jacido/gatewayservice/GatewayServiceApplication.java
@@ -2,9 +2,16 @@ package com.jacido.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
+
+	@Bean
+	public WebProperties.Resources resources() {
+		return new WebProperties.Resources();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
--- gateway-service/src/main/java/com/jacido/gatewayservice/exception/CustomAttributes.java
+++ gateway-service/src/main/java/com/jacido/gatewayservice/exception/CustomAttributes.java
@@ -1,21 +1,19 @@
 package com.jacido.gatewayservice.exception;
 
 import org.springframework.boot.web.error.ErrorAttributeOptions;
-import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.reactive.function.server.ServerRequest;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class CustomAttributes extends DefaultErrorAttributes {
-
     @Override
-    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
-
+    public Map<String, Object> getErrorAttributes(ServerRequest request, ErrorAttributeOptions options) {
         Map<String, Object> errorAttributes = new HashMap<>();
-        Throwable throwable = super.getError(webRequest);
+        Throwable throwable = super.getError(request);
         if (throwable instanceof CustomException) {
             CustomException customException = (CustomException) throwable;
             errorAttributes.put("status", customException.getStatus());
@@ -24,6 +22,4 @@ public class CustomAttributes extends DefaultErrorAttributes {
 
         return errorAttributes;
     }
-
-
 }
